cef286d2898213c56c9eedc5bb98d26709a32135
nor5 <noorbelkacem5@gmail.com>
2022-12-15 18:29:35 +0100

    first commit

---
+++ ProbabilityCalculator.py
@@ -0,0 +1,10 @@
+import copy
+import random
+# Consider using the modules imported above.
+
+class Hat:
+  def __init__(self, **balls):
+    
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
